511c781851d90c586154c4bbaddec1ed3f25ac1a
sandy <sandeep9411@gmail.com>
2020-09-24 12:25:42 +0530

    First commit

---
+++ src/main/java/App/Sum.java
@@ -0,0 +1,10 @@
+package App;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Sum of "+5+" and "+6+"= "+(6+5));
+	}
+
+}
---
+++ src/test/java/Test/Sub.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sub of "+6+" and "+5+"= "+(6-5));
+	}
+
+}
